4a0b72a075306ec16467aeb4ebffc560b1048466
Grant Heaslip <grant@heaslip.me>
2017-06-23 19:25:35 -0400

    Render manifest.json with Django so that it can include cache-busted static asset paths

--- personal/urls.py
+++ personal/urls.py
@@ -1,8 +1,25 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 from .views import IndexView, NotFoundView
 
 app_name = 'personal'
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='personal.index'),
-    url(r'', NotFoundView.as_view(), name='personal.notfound')
+    url(
+        r'^$',
+        IndexView.as_view(),
+        name='personal.index'
+    ),
+    url(
+        r'^static/personal/favicons/manifest.json$',
+        TemplateView.as_view(
+            template_name='personal/manifest.json',
+            content_type='application/json'
+        ),
+        name='personal.manifestjson',
+    ),
+    url(
+        r'',
+        NotFoundView.as_view(),
+        name='personal.notfound'
+    )
 ]
