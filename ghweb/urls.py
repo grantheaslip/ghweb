16879fa432874efabd20cc7d3db60d98a8bdb01c
Grant Heaslip <grant@heaslip.me>
2017-06-25 11:28:24 -0400

    Always redirect (.*)/index.html to (.*)

--- ghweb/urls.py
+++ ghweb/urls.py
@@ -1,8 +1,11 @@
 from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 app_name = 'ghweb'
 urlpatterns = [
+    # Todo: Set permanent=True once this is tested
+    url(r'^(?P<canonical_path>.*)index\.html$', RedirectView.as_view(url='/%(canonical_path)s')),
     url(r'^', include('personal.urls'))
 ]
